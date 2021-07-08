bbd15fc99dde3f86dafa7c05cf8c4be364720797
Le Dinh Tai <taild@vietis.vn>
2021-07-08 17:47:06 +0700

    class base of project

--- app/src/main/java/com/vietis/themovies/MainActivity.kt
+++ app/src/main/java/com/vietis/themovies/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vietis.themovies
+package com.vietis.themovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vietis.themovies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
